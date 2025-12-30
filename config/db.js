1da7649b43d5732be44ecb3b893d12a33b3dc254
Ali Azhar <aliazhar@Alis-MacBook-Air.local>
2025-12-31 04:45:30 +0530

    Fix MongoDB connect options

--- config/db.js
+++ config/db.js
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "shopnow",
+    });
+    console.log("MongoDB connected ✔️");
   } catch (error) {
-    console.error("MongoDB connection failed");
+    console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
 
 export default connectDB;
+
